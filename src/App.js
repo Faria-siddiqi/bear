3afc03c3f1fd39c7159d9e2a045afbb493e582ff
Abdul Saboor <nasirhussain@Nasirs-MacBook-Pro.local>
2022-02-23 14:42:28 -0800

    Team section working now

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import AboutUs from './Screens/AboutUs/AboutUs';
+import Heading from './Screens/Heading/Heading';
+import Home from './Screens/Home/Home';
+import Works from './Screens/Works/Works';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Home>
+      <div>
+        <Heading />
+        <AboutUs />
+        <Works />
+      </div>
+    </Home>
   )
 }
 
---
+++ src/Screens/Heading/Heading.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styles from './Heading.module.scss';
+import { Row, Col } from 'reactstrap';
+
+const Heading = () => {
+    return (
+        <div className={styles.main_heading}>
+            <Row>
+                <Col sm={12} md={12}>
+                    <div className={styles.heading_nav}>
+                        <div className={styles.head}>
+                            <h3>LONELY BEAR <br />SOCIETY</h3>
+                        </div>
+                        <div className={styles.links}>
+                            <p>ABOUT US</p>
+                            <p>WORKS</p>
+                            <p>ARTIST</p>
+                            <p>ROADMAP</p>
+                            <p>FAQ</p>
+                            <p>JOIN</p>
+                        </div>
+                    </div>
+                </Col>
+                <Col sm={12} md={4}>
+                    <div className={styles.bearImg}>
+                        <img src="Assets/bear.png" alt="Bear Pic" />
+                    </div>
+                </Col>
+                <Col sm={12} md={8}>
+                    <div className={styles.bearContent}>
+                        <h1>LONELY BEAR <br />SOCIETY</h1>
+                        <button>JOIN OUR DISCORD</button>
+                    </div>
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+export default Heading;
