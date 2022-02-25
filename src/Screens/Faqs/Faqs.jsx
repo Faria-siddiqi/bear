cb56ff4a60c3b24f40d4cf5bd582a54e284414df
AbdulSaboor100 <abdulsaboor5808@gmail.com>
2022-02-26 03:43:38 +0500

    update fixing

--- src/Screens/Faqs/Faqs.jsx
+++ src/Screens/Faqs/Faqs.jsx
@@ -1,15 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './Faqs.module.scss';
 import { Row, Col } from 'reactstrap';
 import { Collapse } from 'antd';
 import './OverRide.scss';
 import { Animated } from 'react-animated-css';
+import { PlusOutlined } from '@ant-design/icons';
+import { MinusOutlined } from '@ant-design/icons';
 
 const { Panel } = Collapse;
 
 const Faqs = () => {
+  let [keyCheckerState, setKeyCheckerState] = useState();
+
+  const genExtra = () => (
+    <PlusOutlined
+      onClick={event => {
+        // If you don't want click extra trigger collapse, you can prevent this:
+        event.stopPropagation();
+      }}
+    />
+  );
+  const genExtra2 = () => (
+    <MinusOutlined
+      onClick={event => {
+        // If you don't want click extra trigger collapse, you can prevent this:
+        event.stopPropagation();
+      }}
+    />
+  );
   function callback(key) {
-    console.log(key);
+    if (key[1]) {
+      key.shift();
+      setKeyCheckerState(key[0]);
+    } else {
+      setKeyCheckerState(key[0]);
+    }
   }
   const text = `
   A dog is a type of domesticated animal.
@@ -17,56 +42,8 @@ const Faqs = () => {
   it can be found as a welcome guest in many households across the world.
 `;
 
-  // useEffect(() => {
-  //   let div = document.getElementsByClassName('ant-collapse-header')[0];
-  //   console.log(div);
-  //   let head1 = document.createElement('div');
-  //   let h3Node1 = document.createTextNode('+');
-  //   let head2 = document.createElement('div');
-  //   let h3Node2 = document.createTextNode(' ');
-  //   let head3 = document.createElement('div');
-  //   let h3Node3 = document.createTextNode(' ');
-  //   let head4 = document.createElement('div');
-  //   let h3Node4 = document.createTextNode(' ');
-  //   let head5 = document.createElement('div');
-  //   let h3Node5 = document.createTextNode(' ');
-  //   let head6 = document.createElement('div');
-  //   let h3Node6 = document.createTextNode(' ');
-  //   let head7 = document.createElement('div');
-  //   let h3Node7 = document.createTextNode(' ');
-  //   let head8 = document.createElement('div');
-  //   let h3Node8 = document.createTextNode(' ');
-  //   let head9 = document.createElement('div');
-  //   let h3Node9 = document.createTextNode(' ');
-  //   let head10 = document.createElement('div');
-  //   let h3Node10 = document.createTextNode('+');
-
-  //   head1.appendChild(h3Node1);
-  //   head2.appendChild(h3Node2);
-  //   head3.appendChild(h3Node3);
-  //   head4.appendChild(h3Node4);
-  //   head5.appendChild(h3Node5);
-  //   head6.appendChild(h3Node6);
-  //   head6.appendChild(h3Node7);
-  //   head6.appendChild(h3Node8);
-  //   head6.appendChild(h3Node9);
-  //   head6.appendChild(h3Node10);
-
-  //   div.appendChild(head1);
-  //   div.appendChild(head2);
-  //   div.appendChild(head3);
-  //   div.appendChild(head4);
-  //   div.appendChild(head5);
-  //   div.appendChild(head6);
-  //   div.appendChild(head7);
-  //   div.appendChild(head8);
-  //   div.appendChild(head9);
-  //   div.appendChild(head10);
-
-  // }, []);
-
   return (
-    <div className={styles.main_faqs} id="faqss">
+    <div className={styles.main_faqs} id='faqss'>
       <Animated
         isVisible={true}
         animationIn='animate__fadeInUp'
@@ -79,32 +56,58 @@ const Faqs = () => {
             </div>
           </Col>
           <Col sm={12} md={12} id='faqs'>
-            <Collapse defaultActiveKey={['1']} onChange={callback}>
-              <Panel header='What is Lonely Bear Society?' key='1'>
+            <Collapse
+              defaultActiveKey={[]}
+              onChange={callback}
+              expandIconPosition='right'
+            >
+              <Panel
+                header='What is Lonely Bear Society?'
+                key='1'
+                extra={keyCheckerState === '1' ? genExtra2() : genExtra()}
+              >
                 <p>{text}</p>
               </Panel>
-              <Panel header='When will The Lonely Bear Society launch?' key='2'>
+              <Panel
+                header='When will The Lonely Bear Society launch?'
+                key='2'
+                extra={keyCheckerState === '2' ? genExtra2() : genExtra()}
+              >
                 <p>{text}</p>
               </Panel>
-              <Panel header='How can I join' key='3'>
+              <Panel
+                header='How can I join'
+                key='3'
+                extra={keyCheckerState === '3' ? genExtra2() : genExtra()}
+              >
                 <p>{text}</p>
               </Panel>
               <Panel
                 header='IS LONELY BEAR SOCIETY A PROMISING PROJECT?'
                 key='4'
+                extra={keyCheckerState === '4' ? genExtra2() : genExtra()}
               >
                 <p>{text}</p>
               </Panel>
-              <Panel header='Why mental health?' key='5'>
+              <Panel
+                header='Why mental health?'
+                key='5'
+                extra={keyCheckerState === '5' ? genExtra2() : genExtra()}
+              >
                 <p>{text}</p>
               </Panel>
               <Panel
                 header='Why does the body of the bear not match the head?'
                 key='6'
+                extra={keyCheckerState === '6' ? genExtra2() : genExtra()}
               >
                 <p>{text}</p>
               </Panel>
-              <Panel header='WHAT BLOCKCHAIN IS THE PROJECT HOSTED ON?' key='7'>
+              <Panel
+                header='WHAT BLOCKCHAIN IS THE PROJECT HOSTED ON?'
+                key='7'
+                extra={keyCheckerState === '7' ? genExtra2() : genExtra()}
+              >
                 <p>{text}</p>
               </Panel>
             </Collapse>
